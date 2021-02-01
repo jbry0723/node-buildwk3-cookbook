296dda62b67f903250d7f3569812c442629036f4
Justin Justin <jbry0723@gmail.com>
2021-02-01 13:14:34 -0500

    sql config

--- data/migrations/20210122003608_first-migration.js
+++ data/migrations/20210122003608_first-migration.js
@@ -2,14 +2,26 @@ const { table } = require("../db-config");
 
 exports.up = function (knex) {
   return knex.schema
-  .createTable("sources", (tbl) => {
-    tbl.increments("source_id")
-    tbl.string("source_name", 255).notNullable().unique();
-  })
-  .createTable("categories", (tbl) => {
-    tbl.increments("category_id")
-    tbl.string("category_name", 255).notNullable().unique();
-  })
+    .createTable("sources", (tbl) => {
+      tbl.increments("source_id");
+      tbl.string("source_name", 255).notNullable().unique();
+    })
+    .createTable("categories", (tbl) => {
+      tbl.increments("category_id");
+      tbl.string("category_name", 255).notNullable().unique();
+    })
+
+    .createTable("ingredients", (tbl) => {
+      tbl.increments("ingredient_id");
+      tbl.string("ingredient_name", 128).notNullable();
+    })
+   
+    .createTable("users", (tbl) => {
+      tbl.increments("user_id");
+      tbl.string("username", 255).notNullable().unique();
+      tbl.string("password", 255).notNullable();
+    })
+
     .createTable("recipes", (tbl) => {
       tbl.increments("recipe_id");
       tbl.string("recipe_name", 128).notNullable();
@@ -26,10 +38,41 @@ exports.up = function (knex) {
         .inTable("categories")
         .onDelete("CASCADE");
       tbl.string("img_url", 240);
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
     })
-    .createTable("ingredients", (tbl) => {
-      tbl.increments("ingredient_id");
-      tbl.string("ingredient_name", 128).notNullable();
+    .createTable("instructions", (tbl) => {
+      tbl.increments("instruction_id");
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("CASCADE");
+      tbl.string("instruction_text", 600).notNullable();
+      tbl.integer("step_number").unsigned().notNullable();
+    })
+    .createTable("users_recipes", (tbl) => {
+      tbl.primary(["user_id", "recipe_id"]);
+      tbl
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onDelete("CASCADE");
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("CASCADE");
     })
     .createTable("recipe_ingredients", (tbl) => {
       tbl.primary(["recipe_id", "ingredient_id"]);
@@ -50,51 +93,16 @@ exports.up = function (knex) {
       tbl.float("quantity").unsigned().notNullable();
       tbl.string("quantity_units");
     })
-    .createTable("instructions", (tbl) => {
-      tbl.increments("instruction_id");
-      tbl
-        .integer("recipe_id")
-        .unsigned()
-        .notNullable()
-        .references("recipe_id")
-        .inTable("recipes")
-        .onDelete("CASCADE");
-      tbl.string("instruction_text", 600).notNullable();
-      tbl.integer("step_number").unsigned().notNullable();
-    })
-    .createTable("users", (tbl) => {
-      tbl.increments("user_id");
-      tbl.string("username", 255).notNullable().unique();
-      tbl.string("password", 255).notNullable();
-    })
-    .createTable("users_recipes", (tbl) => {
-      tbl.primary(["user_id", "recipe_id"]);
-      tbl
-        .integer("user_id")
-        .unsigned()
-        .notNullable()
-        .references("user_id")
-        .inTable("users")
-        .onDelete("CASCADE");
-      tbl
-        .integer("recipe_id")
-        .unsigned()
-        .notNullable()
-        .references("recipe_id")
-        .inTable("recipes")
-        .onDelete("CASCADE");
-    })
-  
 };
 
 exports.down = function (knex) {
   return knex.schema
+  .dropTableIfExists("recipe_ingredients")
     .dropTableIfExists("users_recipes")
-    .dropTableIfExists("users")
     .dropTableIfExists("instructions")
-    .dropTableIfExists("recipe_ingredients")
-    .dropTableIfExists("ingredients")
     .dropTableIfExists("recipes")
+    .dropTableIfExists("users")
+    .dropTableIfExists("ingredients")
     .dropTableIfExists("categories")
-    .dropTableIfExists("sources")
+    .dropTableIfExists("sources");
 };
--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,33 @@
-require("dotenv").config();
+
+
+const pg=require('pg')
+const localConnection='postgresql://postgres:pgAdminPass@localhost/recipesbook'
+
+let connection
+
+if(process.env.DATABASE_URL){
+  pg.defaults.ssl={rejectUnauthoritized:false}
+  connection=process.env.DATABASE_URL
+} else {
+  connection=localConnection
+}
+
+const sharedConfig={
+  client:'pg',
+  connection,
+  migrations:{directory: './data/migrations'},
+  seeds: {directory: './data/seeds'},
+  
+}
+
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: './data/recipebook.db3'
-    },
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+  development: {...sharedConfig},
+  production:{...sharedConfig,
+    pool: {min:2,max:10
       },
-  },
-
- 
-  
-
-
-
-  }
+     
+      },
 }
+
