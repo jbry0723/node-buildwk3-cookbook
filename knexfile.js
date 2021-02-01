d0a4f80aaa607c428ec0ec1e1930da15724d243d
Justin Justin <jbry0723@gmail.com>
2021-02-01 13:48:14 -0500

    fixed typo in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,24 @@
+const pg = require("pg");
+const localConnection =
+  "postgresql://postgres:pgAdminPass@localhost/recipesbook";
 
+let connection;
 
-const pg=require('pg')
-const localConnection='postgresql://postgres:pgAdminPass@localhost/recipesbook'
-
-let connection
-
-if(process.env.DATABASE_URL){
-  pg.defaults.ssl={rejectUnauthoritized:false}
-  connection=process.env.DATABASE_URL
+if (process.env.DATABASE_URL) {
+  pg.defaults.ssl = { rejectUnauthorized: false };
+  connection = process.env.DATABASE_URL;
 } else {
-  connection=localConnection
+  connection = localConnection;
 }
 
-const sharedConfig={
-  client:'pg',
+const sharedConfig = {
+  client: "pg",
   connection,
-  migrations:{directory: './data/migrations'},
-  seeds: {directory: './data/seeds'},
-  
-}
-
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+};
 
 module.exports = {
-
-  development: {...sharedConfig},
-  production:{...sharedConfig,
-    pool: {min:2,max:10
-      },
-     
-      },
-}
-
+  development: { ...sharedConfig },
+  production: { ...sharedConfig, pool: { min: 2, max: 10 } },
+};
